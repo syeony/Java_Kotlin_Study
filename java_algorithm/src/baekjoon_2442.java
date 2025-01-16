9e5208a51c44d49a918b42594b20ed11e598a231
syeony <fltkfltk1004@naver.com>
2025-01-16 19:21:55 +0900

    240116

--- java_algorithm/src/baekjoon_2442.java
+++ java_algorithm/src/baekjoon_2442.java
@@ -4,14 +4,16 @@ public class baekjoon_2442 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int star = 1;
 
         for(int i=1;i<=n;i++){
-            for(int j=n-i;j>=0;j--){
+            for(int j=n-i;j>0;j--){
                 System.out.print(" ");
             }
-            for(int x=1;x<=i;x++){
+            for(int x=0;x<star;x++){
                 System.out.print("*");
             }
+            star+=2;
             System.out.println();
         }
     }
